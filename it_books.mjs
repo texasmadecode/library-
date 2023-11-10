abf33e0c027f17b3f4d4d718860ee5a8420cc121
texasmadecode <135731172+texasmadecode@users.noreply.github.com>
2023-11-09 20:53:23 -0600

    Update it_books.mjs

--- it_books.mjs
+++ it_books.mjs
@@ -1,23 +1,25 @@
- export function techbooks(){class itbooks{
-    constructor(title,Author,pages) {
-        this._title = title
-        this._Author = Author
-        this._pages = pages
+ class itbooks {
+    constructor(title, Author, pages) {
+        this._title = title;
+        this._Author = Author;
+        this._pages = pages;
     }
 
     get title() {
-        return this._title
+        return this._title;
     }
 
     get Author() {
-        return this._Author
+        return this._Author;
     }
 
     get pages() {
-        return this._pages
+        return this._pages;
     }
 }
-const comptia1 = new itbooks("comptia itf+","James Pengelly","550");
-const comptia2 = new itbooks("comptia core 1 and 2","james pengelly","890");
-const book3 = new itbooks("coding for nerds","n/a","500"); 
- };
+
+const comptia1 = new itbooks("comptia itf+", "James Pengelly", "550");
+const comptia2 = new itbooks("comptia core 1 and 2", "james pengelly", "890");
+const book3 = new itbooks("coding for nerds", "n/a", "500");
+
+export default { comptia1, comptia2, book3 };
